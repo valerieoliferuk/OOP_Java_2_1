627e9feb602665f69a87d13a86a36b5a40946d99
V.Oliferuk <ValeriaOliferuk7789@ukr.net>
2024-11-25 14:48:17 +0200

    Add new method sellInLessThan0() to simplify code

--- Lab3/com.gildedrose/Main.java
+++ Lab3/com.gildedrose/Main.java
@@ -28,6 +28,20 @@ class GildedRose {
         }
     }
 
+    public void sellInLessThan0(){
+        if (!items[i].name.equals("Aged Brie")
+                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert"))
+        {
+            nameNotEqualsAgedBrieOrBackstage();
+        }else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
+            items[i].quality = items[i].quality - items[i].quality;
+        }else if (items[i].name.equals("Aged Brie")){
+            qualityLessThan50();
+
+        }
+
+    }
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
@@ -45,17 +59,10 @@ class GildedRose {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0 && !items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                nameNotEqualsAgedBrieOrBackstage();
+            if (items[i].sellIn < 0){
+                sellInLessThan0();
             }
-
-            if (items[i].sellIn < 0 && items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
-                items[i].quality = items[i].quality - items[i].quality;
-            }
-
-            if (items[i].sellIn < 0 && items[i].name.equals("Aged Brie")) {
-                qualityLessThan50();
-            }
+            
         }
     }
 }
