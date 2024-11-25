b6c778586a0c3862ed1527a362cca48799a41904
V.Oliferuk <ValeriaOliferuk7789@ukr.net>
2024-11-25 14:06:30 +0200

    Apply method nameNotEqualsAgedBrieOrBackstage() instead of the same if

--- Lab3/com.gildedrose/Main.java
+++ Lab3/com.gildedrose/Main.java
@@ -48,9 +48,7 @@ class GildedRose {
 
             if (items[i].sellIn < 0 && !items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                        }
+                        nameNotEqualsAgedBrieOrBackstage();
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
