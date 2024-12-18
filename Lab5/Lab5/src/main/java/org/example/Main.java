6359953bf3ac5309c3307abd733229933f0c5e1f
V.Oliferuk <ValeriaOliferuk7789@ukr.net>
2024-12-18 21:33:40 +0200

    Add Main.java and abstract class Worker.java. At this point there is no need in tests

---
+++ Lab5/Lab5/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        }
+    }
+// to combine this two patterns we need firstly to create classes for work
+// hierarchy and implement composite pattern: abstract class worker,
+// and classes individualWorker, Manager and CEO that implement it.
---
+++ Lab5/Lab5/src/main/java/org/example/Worker.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public abstract class Worker {
+    void getDetails(){
+
+    };
+}
+// this is an abstract class, and moreover it won't have a body in his
+// one method, so it doesn't need unit-tests
